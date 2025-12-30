b6222da35889062feab3d049b5fb87347715d657
Shriharish111 <shriharish111@gmail.com>
2025-12-30 11:39:22 +0530

    Backend Update 2

--- frontend/src/api/trending.api.js
+++
@@ -1,6 +0,0 @@
-import apiClient from "./axios"
-
-export async function fetchTrendingComponents() {
-  const response = await apiClient.get("/trending-components")
-  return response.data
-}
--- frontend/src/components/layout/Container.jsx
+++
@@ -1,9 +0,0 @@
-function Container({ children }) {
-  return (
-    <main className="max-w-5xl mx-auto p-6">
-      {children}
-    </main>
-  )
-}
-
-export default Container
